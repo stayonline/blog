34cdeb27af27a912ef36dbada8ae196524cf3791
Jim Smits <james.smits@gmail.com>
2025-08-05 12:42:32 -0400

    Add image asset synchronization to sync_obsidian.sh

--- sync_obsidian.sh
+++ sync_obsidian.sh
@@ -4,5 +4,9 @@
 SRC="/mnt/c/Users/JimSmits/OneDrive - StayOnline/Documents/Obsidian Vaults/stayoline-wire/_posts"
 DEST="/home/jim/stayonline-wire/_posts"
 
+IMGSRC="/mnt/c/Users/JimSmits/OneDrive - StayOnline/Documents/Obsidian Vaults/stayoline-wire/assets"
+IMGDEST="/home/jim/stayonline-wire/assets"
+
 # Sync files without deleting anything
 rsync -av "$SRC/" "$DEST/"
+rsync -av "$IMGSRC/" "$IMGDEST/"
